4041b098b826e2139ce224dbcc9d4c54cbf5d7b0
Alejandro <alexgb11@gmail.com>
2020-11-01 16:20:49 +0100

    Mapa copiado de la practica de Ruben (profesor). Muestra coordenadas en medio del oceano.

---
+++ app/src/main/java/net/juanxxiii/practica1t/MapActivity.java
@@ -0,0 +1,72 @@
+package net.juanxxiii.practica1t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+import static net.juanxxiii.practica1t.HomeActivity.DESCRIPTION_KEY;
+import static net.juanxxiii.practica1t.HomeActivity.TITLE_KEY;
+import static net.juanxxiii.practica1t.common.Constants.INTENT_LOCALIZATION_ACTION;
+import static net.juanxxiii.practica1t.common.Constants.LATITUDE;
+import static net.juanxxiii.practica1t.common.Constants.LONGITUDE;
+
+public class MapActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getName();
+    private MapView mMapView;
+    private MapController mMapController;
+    GeoPoint geoPointMyPosition;
+    private ArrayList<OverlayItem> mOverlayItems = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        Intent getDataIntent = getIntent();
+        geoPointMyPosition = new GeoPoint(getDataIntent.getDoubleExtra(LATITUDE, 0), getDataIntent.getDoubleExtra(LONGITUDE, 0));
+        generateOpenStreetMapViewAndMapController();
+
+        boolean add = mOverlayItems.add(new OverlayItem(getDataIntent.getStringExtra(TITLE_KEY), getDataIntent.getStringExtra(DESCRIPTION_KEY), geoPointMyPosition));
+
+
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(mOverlayItems, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return false;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        }, getApplicationContext());
+
+        mOverlay.setFocusItemsOnTap(true);
+        mMapView.getOverlays().add(mOverlay);
+    }
+
+
+    public void generateOpenStreetMapViewAndMapController() {
+        mMapView = (MapView) findViewById(R.id.openmapview);
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        mMapController = (MapController) mMapView.getController();
+        mMapController.setZoom(18);
+        mMapController.setCenter(geoPointMyPosition);
+    }
+
+}
---
+++ app/src/main/java/net/juanxxiii/practica1t/common/Constants.java
@@ -0,0 +1,7 @@
+package net.juanxxiii.practica1t.common;
+
+public class Constants {
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+    public static final String INTENT_LOCALIZATION_ACTION = "location-event-position";
+}
